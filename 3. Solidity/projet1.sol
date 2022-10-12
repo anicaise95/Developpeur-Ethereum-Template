f67ff0ed9588a2e625971d43bd5aa3041a6e2e06
anicaise95 <112395282+anicaise95@users.noreply.github.com>
2022-10-12 13:56:21 +0200

    Update projet1.sol
    Finale version

--- 3. Solidity/projet1.sol
+++ 3. Solidity/projet1.sol
@@ -3,19 +3,27 @@
 pragma solidity ^0.8.17;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
+// Projet - Système de vote
 contract Voting is Ownable {
 
+    // Votant
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
     }
+    // Liste des votants
+    mapping(address => Voter) voters;
 
+    // Proposition de vote
     struct Proposal {
         string description;
         uint voteCount;
     }
+    // Liste des propositions
+    Proposal[] proposals;
 
+    // Statuts session de vote
     enum WorkflowStatus {
         RegisteringVoters,
         ProposalsRegistrationStarted,
@@ -23,27 +31,27 @@ contract Voting is Ownable {
         VotingSessionStarted,
         VotingSessionEnded,
         VotesTallied
-    }    
+    }   
+    // Statut actuel de la session de vote 
+    WorkflowStatus workflowStatus = WorkflowStatus.RegisteringVoters;
 
+    // events
     event VoterRegistered(address voterAddress);
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
 
-    mapping(address => Voter) voters;
-    WorkflowStatus workflowStatus = WorkflowStatus.RegisteringVoters;
-    Proposal[] proposals;
-    
     // Propostions ayant le plus votes (permet d'identifier les potentielles égalités)
-    uint nbMaxVotes = 0; 
+    uint nbMaxVotes = 0;
+    // Liste du gagnant - eventuellement des gagnants en cas d'égalité
     Proposal[] winningProposals;
 
     constructor(){ 
         workflowStatus = WorkflowStatus.RegisteringVoters;
     }
 
-    // update le statut du workflow
-    function updateWorkflowStatus(WorkflowStatus newStatus) private {
+    // Update du statut de la session de vote
+    function _updateWorkflowStatus(WorkflowStatus newStatus) private {
         emit WorkflowStatusChange(workflowStatus, newStatus);
         workflowStatus = newStatus;
     }
@@ -55,96 +63,103 @@ contract Voting is Ownable {
         emit VoterRegistered(_address);
     }
 
-    // Voir les informations d'un voteur
+    // Retourne les détails d'un voteur
     function getVoter(address _address) view public returns (Voter memory) {
         return voters[_address];
     }
 
-    // L'administrateur du vote commence la session d'enregistrement de la proposition
+    // Débuter la session d'enregistrement de la proposition
     function startProposalsRegistration() public onlyOwner {
         require(workflowStatus == WorkflowStatus.RegisteringVoters, unicode"Il faut d'abord inscrire les électeurs");
-        updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted);
+        _updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted);
     }
 
-    // L'administrateur du vote stoppe la session d'enregistrement de la proposition
+    // Cloturer la session d'enregistrement de la proposition
     function stopProposalsRegistration() public onlyOwner {
         require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "La session d'enregistrement des propositions n'est pas encore ouverte");
-        updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationEnded);
+        _updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationEnded);
     }
 
-    // L'administrateur débute la session des votes
+    // Débuter la session des votes
     function startVotingSession() public onlyOwner {
         require(workflowStatus == WorkflowStatus.ProposalsRegistrationEnded, "La session d'enregistrement des propositions doit etre fermee");
+
         // Ici je vérifie la présence d'au moins 2 propositions pour pouvoir voter
         if(proposals.length <= 2){
-            updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted);
+            _updateWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted);
             revert("Un minimum de 2 propositions est requis pour voter");
         }
-        updateWorkflowStatus(WorkflowStatus.VotingSessionStarted);
+        _updateWorkflowStatus(WorkflowStatus.VotingSessionStarted);
     }
 
-    // L'administrateur stoppe la session des votes
+    // Cloturer la session des votes
     function stopVotingSession() public onlyOwner {
         require(workflowStatus == WorkflowStatus.VotingSessionStarted, "La session de vote doit etre ouverte");
-        updateWorkflowStatus(WorkflowStatus.VotingSessionEnded);
+        _updateWorkflowStatus(WorkflowStatus.VotingSessionEnded);
     }  
 
-    // Liste des propositions 
+    // Retourne la liste des propositions 
     function getProposals() public view returns(Proposal[] memory)  {
         return proposals;
     }
 
+    // Le caller doit être enregistré comme électeur pour réaliser l'action
     modifier isRegistered() {
         require(voters[msg.sender].isRegistered, unicode"Vous n'êtes pas inscrit sur la liste de vote");
         _;
     }   
   
-    // Ajout de la proposition de l'électeur
-    function addproposal(string memory _proposal) public isRegistered {
-        require(bytes(_proposal).length > 0, "Aucune proposition renseignee."); 
+    // Ajouter une proposition
+    function addproposal(string memory _newProposalDescription) public isRegistered {
+        require(bytes(_newProposalDescription).length > 0, "Aucune proposition renseignee"); 
         require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "La session d'enregistrement des propositions est fermee"); 
+
         // La proposition est ajoutée si pas déjà présente (éviter les doublons)
         for(uint i = 0; i < proposals.length; i++){
-            if (keccak256(abi.encodePacked(proposals[i].description)) == keccak256(abi.encodePacked(_proposal))) {
+            if (keccak256(abi.encodePacked(proposals[i].description)) == keccak256(abi.encodePacked(_newProposalDescription))) {
                 revert("Une proposition existe deja");
             }
         }
-        proposals.push(Proposal(_proposal, 0));
+
+        proposals.push(Proposal(_newProposalDescription, 0));
         emit ProposalRegistered(proposals.length);
     }
 
     // Les électeurs inscrits votent pour leur proposition préférée.
-    // POUR VOTER saisir la position de la proposition dans le tableau à partir de 0
-   function vote(uint _votedProposalId) public isRegistered {
-        require(_votedProposalId >= 0, "Vote invalide");
+    // POUR VOTER saisir la position de la proposition dans le tableau (à partir de 0)
+   function vote(uint _indexVotedProposalId) public isRegistered {
+        require(_indexVotedProposalId >= 0, "Vote invalide");
         require(workflowStatus == WorkflowStatus.VotingSessionStarted, "La session de vote n'est pas encore ouverte");
-        require(!voters[msg.sender].hasVoted, unicode"Votre vote a déjà été pris en compte");
-        voters[msg.sender] = Voter(true, true, _votedProposalId);
-        proposals[_votedProposalId].voteCount += 1;
-        emit Voted(msg.sender, _votedProposalId);
+        require(!voters[msg.sender].hasVoted, "Votre vote a deja ete pris en compte");
+
+        voters[msg.sender] = Voter(true, true, _indexVotedProposalId);
+        proposals[_indexVotedProposalId].voteCount += 1;
+        emit Voted(msg.sender, _indexVotedProposalId);
     }
 
-    // Dépouillemement
-    function countVoteees() public onlyOwner {
+    // Dépouillemement des votes
+    function countVotes() public onlyOwner {
         require(workflowStatus == WorkflowStatus.VotingSessionEnded, "La session de vote est toujours ouverte");
-        updateWorkflowStatus(WorkflowStatus.VotesTallied);
+        _updateWorkflowStatus(WorkflowStatus.VotesTallied);
 
-        // Propostions ayant le plus votes (permet d'identifier les potentielles égalités)
+        // Le gagnant ou les gagnants (en cas d'égalité) avec le plus de votes sont copiés dans un tableau
         for (uint i = 0; i < proposals.length; i++){
             if (proposals[i].voteCount >= nbMaxVotes){
+                // Si une propososition a un nombre de votes plus important, on réinitialise le tableau des gagnants
+                if (proposals[i].voteCount > nbMaxVotes){
+                    delete winningProposals;
+                }
+                // On conserve le nombre de votse le plus important
                 nbMaxVotes = proposals[i].voteCount;
+                // On enregistre la proposition gagante ou les propositions gaghnantes en cas d'égalité
+                winningProposals.push(proposals[i]);
             }
         }
     }   
 
-    // Tout le monde peut voir le gagnant ou de la proposition
-    function getWinner() public returns(Proposal[] memory) {
+    // Retourne le gagnant (éventuellemetnt les gagnants en cas d'égalité)
+    function getWinner() public view returns(Proposal[] memory) {
         require(workflowStatus == WorkflowStatus.VotesTallied, "Le nom du gagnant n'est pas disponible");
-        for (uint i = 0; i < proposals.length; i++){
-            if (proposals[i].voteCount == nbMaxVotes){
-                winningProposals.push(proposals[i]);
-            }
-        }
         return winningProposals;
     }
 }
