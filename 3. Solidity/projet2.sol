75d7b905114e8f9a4bc9edf5802bd8daa6c903e0
anicaise95 <112395282+anicaise95@users.noreply.github.com>
2022-10-28 00:59:26 +0200

    Update projet2.sol

--- 3. Solidity/projet2.sol
+++ 3. Solidity/projet2.sol
@@ -3,78 +3,85 @@ const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { assertion } = require('@openzeppelin/test-helpers/src/expectRevert');
 const { expect } = require('chai');
 
+
 contract("Voting", accounts => {
 
     const _owner = accounts[0];
-    const adress_voter1 = accounts[1];
-    const adress_voter2 = accounts[2];
-    const adress_voter3 = accounts[3];
+    const address_voter1 = accounts[1];
+    const address_voter2 = accounts[2];
+    const address_voter3 = accounts[3];
+    const address_autre = accounts[4];
 
+    let workflowStatus;
     let votingContractInstance;
 
     describe("Ajout d'électeurs sur la whitelist", function () {
 
-        before(async function () {
+        beforeEach(async function () {
             votingContractInstance = await Voting.new({ from: _owner });
         });
 
-        it("Should be OK .... Ajout de 3 nouveaux électeurs", async () => {
+        it("Check le worflow initial => RegisteringVoters", async () => {
+            workflowStatus = await votingContractInstance.workflowStatus.call();
+            expect(workflowStatus).to.be.bignumber.equal(new BN(0));
+        });
 
-            // Ajout d'un électeur par l'admin
-            expectEvent(await votingContractInstance.addVoter(adress_voter1, { from: _owner }), "VoterRegistered", { voterAddress: adress_voter1 });
-            const voter = await votingContractInstance.getVoter(adress_voter1, { from: adress_voter1 });
+        it("Seul l'administrateur est habilité à ajouter un électeur", async () => {
+            await expectRevert(votingContractInstance.addVoter(address_voter1, { from: address_voter1 }), "caller is not the owner");
+        });
+
+        it("L'administrateur ajoute 2 voters, check de leur présence sur la whitelist", async () => {
+            expectEvent(await votingContractInstance.addVoter(address_voter1, { from: _owner }), "VoterRegistered", { voterAddress: address_voter1 });
+            expectEvent(await votingContractInstance.addVoter(address_voter2, { from: _owner }), "VoterRegistered", { voterAddress: address_voter2 });
+
+            const voter = await votingContractInstance.getVoter(address_voter1, { from: address_voter1 });
             expect(voter.isRegistered).to.be.true;
             expect(voter.hasVoted).to.be.false;
             expect(new BN(voter.votedProposalId)).to.be.bignumber.equal(new BN(0));
 
-            // Ajout d'un électeur par l'admin
-            expectEvent(await votingContractInstance.addVoter(adress_voter2, { from: _owner }), "VoterRegistered", { voterAddress: adress_voter2 });
-            const voter2 = await votingContractInstance.getVoter(adress_voter2, { from: adress_voter2 });
+            const voter2 = await votingContractInstance.getVoter(address_voter2, { from: address_voter2 });
             expect(voter2.isRegistered).to.be.true;
             expect(voter2.hasVoted).to.be.false;
             expect(new BN(voter2.votedProposalId)).to.be.bignumber.equal(new BN(0));
-
-            // Ajout d'un électeur par l'admin
-            expectEvent(await votingContractInstance.addVoter(adress_voter3, { from: _owner }), "VoterRegistered", { voterAddress: adress_voter3 });
-            const voter3 = await votingContractInstance.getVoter(adress_voter3, { from: adress_voter3 });
-            expect(voter3.isRegistered).to.be.true;
-            expect(voter3.hasVoted).to.be.false;
-            expect(new BN(voter3.votedProposalId)).to.be.bignumber.equal(new BN(0));
-
         });
 
-        it("Revert si ajout d'un électeur existant", async () => {
-            await expectRevert(votingContractInstance.addVoter(adress_voter1, { from: _owner }), "Already registered");
+        it("Un électeur ne peut pas être ajouté 2 fois", async () => {
+            await votingContractInstance.addVoter(address_voter1, { from: _owner });
+            await expectRevert(votingContractInstance.addVoter(address_voter1, { from: _owner }), "Already registered");
         });
     });
+
 
     describe("Ajout de propositions", function () {
-        describe("Test des event et require", function () {
-            // Uniquement pour les voteurs
-            it("Should be KO .. if admin", async () => {
-                await expectRevert(votingContractInstance.addProposal("", { from: _owner }), "You're not a voter");
-            });
-            // La session d'enregistrement n'est pas encore ouverte
-            it("Should be KO .. Proposals are not allowed yet ! ", async () => {
-                await expectRevert(votingContractInstance.addProposal("MARSEILLE", { from: adress_voter1 }), "Proposals are not allowed yet");
-            });
 
-
-            it("KO .. Proposal can't be empty ! ", async () => {
-                await expectRevert(votingContractInstance.addProposal("", { from: adress_voter1 }), "Vous ne pouvez pas ne rien proposer");
-            });
-
+        beforeEach(async function () {
+            votingContractInstance = await Voting.new({ from: _owner });
         });
 
-        describe("Ouverture de la session d'enregistrement", function () {
-            it("Should be KO .. Registering proposals can't be started now", async () => {
-                const event = await votingContractInstance.startProposalsRegistering({ from: _owner });
-                //expectEvent(event, "WorkflowStatusChange", { newStatus: "ProposalsRegistrationStarted" });
-            });
+        it("Un électeur absent de la whitelist ne peut pas ajouter de proposition", async () => {
+            await expectRevert(votingContractInstance.addProposal("MARSEILLE", { from: address_voter1 }), "You're not a voter");
         });
 
-        describe("Ajout de n propositions", function () {
-
+        it("Un électeur ne peut pas ajouter une proposition avant l'ouverture de la session", async () => {
+            await votingContractInstance.addVoter(address_voter1, { from: _owner });
+            await expectRevert(votingContractInstance.addProposal("MARSEILLE", { from: address_voter1 }), "Proposals are not allowed yet");
         });
+
+        it("Un électeur ne peut pas ajouter une proposition vide", async () => {
+            await votingContractInstance.addVoter(address_voter1, { from: _owner });
+            await votingContractInstance.startProposalsRegistering({ from: _owner });
+            await expectRevert(votingContractInstance.addProposal("", { from: address_voter1 }), "Vous ne pouvez pas ne rien proposer");
+        });
+
+        it("Un électeur ajoute la proposition MARSEILLE", async () => {
+            await votingContractInstance.addVoter(address_voter1, { from: _owner });
+            await votingContractInstance.startProposalsRegistering({ from: _owner });
+            expectEvent(await votingContractInstance.addProposal(address_voter1, { from: address_voter1 }), "ProposalRegistered", { proposalId: new BN(1) });
+            console.log(await (votingContractInstance.getOneProposal(1, { from: address_voter1 }).description));
+            //expect(proposition).equal("MARSEILLE");
+        });
+
+
     });
+
 });
